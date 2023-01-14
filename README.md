d654ed2f77882e508f829391225feac1aaea3f67
Reny Correia <renylmg@gmail.com>
2023-01-14 16:46:14 -0300

    Versionamento do projeto

---
+++ README.md
@@ -0,0 +1,14 @@
+# Desafio de Automação APIs REST - Base2 
+  
+Projeto realizado para executar o proposto no Desafio Automação - APIs REST da Base2.
+
+## SUT (Software Under Test)
+[Mantis Bug Tracker REST API](https://documenter.getpostman.com/view/29959/mantis-bug-tracker-rest-api/7Lt6zkP#intro)
+
+## Tecnologias Utilizadas 
+- [Docker](https://www.docker.com/) - Ferramenta para levantar containers através de imagens (mantis e act)
+- [MantisBT bug tracker Docker image](https://github.com/okainov/mantisbt-docker)
+- [Postman](https://learning.postman.com/docs/getting-started/introduction)
+- [ACT](https://github.com/nektos/act) - Imagem docker para simular a integracão contínua do projeto na pipeline do Github Actions
+- [Newman Reports](https://learning.postman.com/docs/running-collections/using-newman-cli/) - Biblioteca para gerar relatórios de testes
+- [NodeJs](https://nodejs.org/en/docs/)
---
+++ index.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const async = require('async')
+const newman = require('newman')
+
+const PARALLEL_RUN_COUNT = 2
+
+const parametersForTestRun = {
+    collection: path.join(__dirname, 'postman/desafio-automacao-api.postman_collection.json'),
+    environment: path.join(__dirname, 'postman/desafio-automacao-api-dev.postman_environment.json'),
+    iterationData: path.join(__dirname, 'files/users.csv'),
+    reporters: ['cli', 'htmlextra'],
+    reporter:{
+        htmlextra: {
+            export: './report.html'
+        }
+    }
+};
+
+parallelCollectionRun = function (done) {
+    newman.run(parametersForTestRun, done);
+};
+
+let commands = []
+for (let index = 0; index < PARALLEL_RUN_COUNT; index++) {
+    commands.push(parallelCollectionRun);
+}
+
+async.parallel(
+    commands,
+    (err, results) => {
+        err && console.error(err);
+
+        results.forEach(function (result) {
+            var failures = result.run.failures;
+            console.info(failures.length ? JSON.stringify(failures.failures, null, 2) :
+                `${result.collection.name} ran successfully.`);
+        });
+    }
+);
